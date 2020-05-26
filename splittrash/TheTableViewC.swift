c59a9ae64cd9208ea2e3b13bfaba783fec0ecd97
Michael Redig <mredig@gmail.com>
2020-05-26 18:17:07 -0500

    initial comimt

---
+++ splittrash/TheTableViewC.swift
@@ -0,0 +1,9 @@
+//
+//  TheTableViewC.swift
+//  splittrash
+//
+//  Created by Michael Redig on 5/26/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
---
+++ splittrash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  splittrash
+//
+//  Created by Michael Redig on 5/26/20.
+//  Copyright © 2020 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
